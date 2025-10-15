2d09941328a4bf72bf7e20ac691097099c895450
keixlu <keixlu1000@gmail.com>
2025-10-15 15:52:28 +0800

    Landing Page BYOD progress

---
+++ landing/landing_script.js
@@ -0,0 +1,7 @@
+const sideNavButton = document.getElementById("ToggleSideNavButton");
+const sideNav = document.getElementById("SideNav");
+
+sideNavButton.addEventListener("click", function(){
+    sideNav.classList.toggle("expanded");
+    console.log("Clicked");
+})``
