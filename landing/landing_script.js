5d76da5cb27097aeb4a971a76b2454f8389d8bcf
Eduardo Sabado <2241904@slu.edu.ph>
2025-10-16 01:25:14 +0800

    Saving current Landing Page progress

--- landing/landing_script.js
+++ landing/landing_script.js
@@ -4,4 +4,4 @@ const sideNav = document.getElementById("SideNav");
 sideNavButton.addEventListener("click", function(){
     sideNav.classList.toggle("expanded");
     console.log("Clicked");
-})``
+});
