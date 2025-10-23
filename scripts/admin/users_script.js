90600abe98ddaffe9b9fb1e911573c1aaf3371cb
Fundomizer <jesusjroway@gmail.com>
2025-10-23 16:26:15 +0800

    feat: Added a way to dynamically load in a page into the content

---
+++ scripts/admin/users_script.js
@@ -0,0 +1,7 @@
+const addUserButton = document.getElementById("AddUserButton")
+
+function createUserRow(name, email, role, created, action) {
+
+}
+
+addUserButton.addEventListener("click", () => console.log("Nigga!"))
--- scripts/init_page.js
+++ scripts/init_page.js
@@ -54,17 +54,61 @@ function loadNav(userType = "admin") {
     sideNavContent[0].appendChild(list);
 }
 
-function loadPage(page) {
-    fetch(page)
+/**
+ * Dynamically loads and injects a HTML file into the container, "Content".
+ * @param {String} role User type. Should match the directory name. Values should be "admin", "org", or "osa".
+ * @param {String} page HTML page being loaded into the container, "Content".
+ * @param {String} style File name of the style sheet the page uses, it should include the file exntension.
+ * @param {String} script File name of the script the page uses, it should include the file extension.
+ * @param {String} loadInto Default as "Content". ID of the container to load the page into
+ */
+function loadPage(role, page, style, script, loadInto = "Content") {
+
+    let pathToPage = `../pages/${role}/${page}`
+    let pathToScript = `../scripts/${role}/${script}`
+    let pathToStyle = `../styles/${role}/${style}`
+
+    fetch(pathToPage)
         .then(result => result.text()) // Convert into html text
-        .then(html => document.getElementsByClassName("Content")[0].innerHTML = html);
+        .then(htmlText => {
+            // Parse and grab the #PageContent only to ensure that you don't get the Head tag from the HTML element
+            const parser = new DOMParser()
+            const html = parser.parseFromString(htmlText, "text/html")
+            const mainContent = html.querySelector("#PageContent");
+            document.getElementById(loadInto).innerHTML = mainContent.innerHTML
+
+            // Load CSS and Script into assigned to that page
+            if (pathToStyle) loadCSS(pathToStyle);
+            if (pathToScript) loadScript(pathToScript);
+        });
+
+    function loadScript(src) {
+        if (!document.querySelector(`script[src="${src}"]`)) {
+            const script = document.createElement("script");
+            script.src = src;
+            script.defer = true;
+            document.body.appendChild(script);
+        }
+    }
+
+    function loadCSS(href) {
+        if (!document.querySelector(`link[href="${href}"]`)) {
+            const link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = href;
+            document.head.appendChild(link);
+        }
+    }
+
 }
 
-loadNav()
+// initalise side bar buttons
 
 sideNavButton.addEventListener("click", function () {
     sideNav.classList.toggle("expanded");
     console.log("Clicked");
 });
 
-loadPage("../pages/admin/users_page.html")
+// TODO change this later. Add a better way to dynamically change to the different views later on
+loadNav()
+loadPage( "admin", "users_page.html", "users_style.css", "users_script.js")
