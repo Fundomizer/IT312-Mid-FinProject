676f2fea001aba9a7a130cfe7c45f2fd5335797f
LIEFLANDER LUKE <2241741@slu.edu.ph>
2025-10-28 19:35:59 +0800

    minor: added config file for database

--- scripts/osa/overview_script.js
+++ scripts/osa/overview_script.js
@@ -1,5 +1,7 @@
+import { API_BASE_URL } from "../config.js";
+
 async function loadOrganizations() {
-  const response = await fetch("http://localhost:3000/api/student_organization");
+const response = await fetch(`${API_BASE_URL}/api/student_organization`);
   const data = await response.json();
   const orgContainer = document.getElementById("organizations-list");
   const subContainer = document.getElementById("submissions-list");
