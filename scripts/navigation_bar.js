8e7611d76b5c9b0f907695775b1bd091b7fccaca
Fundomizer <jesusjroway@gmail.com>
2025-11-01 17:11:03 +0800

    fix: Minor fixes to the top bar

--- scripts/navigation_bar.js
+++ scripts/navigation_bar.js
@@ -1,8 +1,13 @@
 const sideNavContent = document.getElementsByClassName("SideNavContent")
 const sideNavButton = document.getElementById("ToggleSideNavButton");
 const sideNav = document.getElementById("SideNav");
+const logoutButton = document.getElementById("Logout")
 
 sideNavButton.addEventListener("click", function () {
     sideNav.classList.toggle("expanded");
     console.log("Clicked");
 });
+
+logoutButton.addEventListener('click', () => {
+    window.location.href = "/index.html";
+})
