1aa60cf6055278e552cf07e4bf7d0f204bf04ad8
Fundomizer <jesusjroway@gmail.com>
2025-11-03 07:34:05 +0800

    fix: Added endpoints for the other sample data

--- server.js
+++ server.js
@@ -15,8 +15,12 @@ const db = client.db("OrganizationManagementDatabase");
 const orgs = db.collection("student_organization");
 const users = db.collection("users");
 const forms = db.collection("forms");
+const history = db.collection("history");
+const org_forms = db.collection("org_forms");
+const osa_submissions = db.collection("osa_submissions");
+const log = db.collection("log");
 
-
+// Expose endpoints
 app.get("/api/users", async (req, res) => {
   const all = await users.find().toArray();
   res.json(all);
@@ -32,7 +36,34 @@ app.get("/api/forms", async (req, res) => {
   res.json(all);
 });
 
+app.get("/api/history", async (req, res) => {
+  const all = await history.find().toArray();
+  res.json(all);
+});
 
+app.get("/api/org_forms", async (req, res) => {
+  const all = await org_forms.find().toArray();
+  res.json(all);
+});
+
+app.get("/api/osa_submissions", async (req, res) => {
+  const all = await osa_submissions.find().toArray();
+  res.json(all);
+});
+
+app.get("/api/log", async (req, res) => {
+  const all = await log.find().toArray();
+  res.json(all);
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Users can be accessed at http://localhost:${PORT}/api/users`);
+  console.log(`Student Organization can be accessed at http://localhost:${PORT}/api/student_organization`);
+  console.log(`Forms can be accessed at http://localhost:${PORT}/api/forms`);
+  console.log(`History can be accessed at http://localhost:${PORT}/api/history`);
+  console.log(`Org Forms can be accessed at http://localhost:${PORT}/api/org_forms`);
+  console.log(`OSA Submissions can be accessed at http://localhost:${PORT}/api/osa_submissions`);
+  console.log(`Log can be accessed at http://localhost:${PORT}/api/log`);
+});
