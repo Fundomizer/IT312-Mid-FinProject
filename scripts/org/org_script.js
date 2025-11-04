a1db60f70560189ba1b4c07c14cf14b993fd1a25
Fundomizer <jesusjroway@gmail.com>
2025-11-04 09:17:28 +0800

    Feat: Added dynamic loading for organisation assigned forms

--- scripts/org/org_script.js
+++ scripts/org/org_script.js
@@ -1,10 +1,10 @@
-import { loadPage } from "../utilities.js"
+import { fetchCollection, loadPage } from "../utilities.js"
 
 
 const dashboard = document.getElementById("dashboardButton")
 const forms = document.getElementById("formsButton")
 const history = document.getElementById("historyButton")
-const popupButtons = document.querySelectorAll(".StyledButton");
+// const popupButtons = document.querySelectorAll(".StyledButton");
 let popup = document.getElementsByClassName("PopupForm");
 const cancel = document.getElementById("CancelForm");
 
@@ -12,8 +12,50 @@ function loadDashboard() {
     loadPage('org', 'dashboard_page.html')
 }
 
-function loadForms() {
+async function loadForms() {
     loadPage('org', 'assigned_form_page.html')
+
+    let forms = await fetchCollection('org_forms')
+
+    displayForm(forms)
+
+    function displayForm(forms) {
+        const formDisplay = document.getElementById('Forms')
+
+        forms.forEach(item => {
+            formDisplay.appendChild(createForm(item))
+        });
+    }
+
+    function createForm(form) {
+        const wrapper = document.createElement("div");
+        wrapper.className = "SubCard Form";
+
+        // Build the inner HTML
+        wrapper.innerHTML = `
+      <div class="FormDetails">
+      <span>
+        <h3 class="FormTitle">${form["title"]}</h3>
+        <p class="Tag">${form["action"]}</p>
+      </span>
+      <p class="FormDescription">${form["activity"]}</p>
+      <div class="FormRequirements">
+        <span>
+          <div class="ImageWrapper">
+            <img src="../../assets/images/org_icons/document_icon.png" alt="Document icon">
+          </div>
+          <p>${form["field_num"]} Fields required</p>
+        </span>
+      </div>
+      <div class="Tags">
+        ${form["tags"].map(tag => `<p class="Tag">${tag}</p>`).join("")}
+      </div>
+    </div>
+  `;
+
+        return wrapper;
+    }
+
 }
 
 function loadHistory() {
@@ -34,7 +76,7 @@ function closeForm() {
 dashboard.addEventListener('click', loadDashboard)
 forms.addEventListener('click', loadForms)
 history.addEventListener('click', loadHistory)
-popupButtons.addEventListener('click', openForm)
-cancel.addEventListener('click', closeForm)
+// popupButtons.addEventListener('click', openForm)
+// cancel.addEventListener('click', closeForm)
 
 loadDashboard() // Load dashboard by default
