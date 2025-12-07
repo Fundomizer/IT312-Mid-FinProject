c01c9d7b25d84b3f9f22289523b17d9abd6022ac
Fundomizer <jesusjroway@gmail.com>
2025-12-07 15:10:10 +0800

    fix: Fixed "cannot use import outside a module"

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,4 +1,4 @@
-import { HOST } from "./config";
+import { HOST, API_BASE_URL } from "./config";
 
 /**
  * Dynamically loads and injects a HTML file into the container, "Content". Provide the file name for the style and script if you intend to use a different file for them.
