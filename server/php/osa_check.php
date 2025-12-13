350ae4ea6cbe7cc3489be007dc00c1bc066db576
LIEFLANDER LUKE <2241741@slu.edu.ph>
2025-12-13 17:08:01 +0800

    removed node from osa side, and lengthened expiration time

--- server/php/osa_check.php
+++ server/php/osa_check.php
@@ -3,7 +3,7 @@ ini_set('session.cookie_lifetime', 0);
 session_start();
 header("Content-Type: application/json");
 // Session timeout check (3 minutes of inactivity)
-$timeout = 180; 
+$timeout = 360; 
 
 if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout) {
     echo json_encode([
