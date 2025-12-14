41abdc46d48b78b5a64620913d82449fb82887d8
LIEFLANDER LUKE <2241741@slu.edu.ph>
2025-12-14 22:01:09 +0800

    made search for osa same as org

--- scripts/navigation_bar.js
+++ scripts/navigation_bar.js
@@ -4,7 +4,7 @@ const sideNavButton = document.getElementById("ToggleSideNavButton");
 const sideNav = document.getElementById("SideNav");
 const logoutButton = document.getElementById("Logout");
 const usernameLabel = document.getElementById('UsernameLabel');
-usernameLabel.innerText = localStorage.getItem("username") || 'OSA';
+usernameLabel.innerText = localStorage.getItem("username") || "";
 
 // Toggle SideNav + overlay
 sideNavButton.addEventListener("click", () => {
